7d0f86d5069819773f532ff4ffb43d79b10c02e2
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-13 23:21:02 +0300

    DAL Process

--- DataAccessLayer/Abstract/ITestimonialDal.cs
+++ DataAccessLayer/Abstract/ITestimonialDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ITestimonial : IGenericDal<Testimonial>
+    public interface ITestimonialDal : IGenericDal<Testimonial>
     {
     }
 }
