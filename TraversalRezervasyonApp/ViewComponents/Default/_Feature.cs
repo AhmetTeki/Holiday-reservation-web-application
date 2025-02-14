eecb9bdae7e4604d97f995d1e6cd851af37d91af
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-14 23:00:06 +0300

    Feature Statistics

---
+++ TraversalRezervasyonApp/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalRezervasyonApp.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager _featureManager = new FeatureManager(new EfFeatureDal());
+
+        
+        public IViewComponentResult Invoke()
+        {
+            var values = _featureManager.TGetList();
+            
+            return View(values);
+        }
+    }
+}
