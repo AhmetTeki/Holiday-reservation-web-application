9d2a099408061c91a27d90ac38784ebafb4078d7
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-14 19:44:37 +0300

    Proje Tur Rotalarının Listelenmesi

---
+++ TraversalRezervasyonApp/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalRezervasyonApp.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        readonly DestinationManager destinationManager = new DestinationManager(new EfDestination());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
