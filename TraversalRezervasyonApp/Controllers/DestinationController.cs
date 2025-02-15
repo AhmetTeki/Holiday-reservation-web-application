13ddb699640ff12fa3323b310095a48b40c1686b
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-15 20:35:48 +0300

    Design

---
+++ TraversalRezervasyonApp/Controllers/DestinationController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalRezervasyonApp.Controllers
+{
+    public class DestinationController : Controller
+    {
+        readonly DestinationManager destinationManager = new DestinationManager(new EfDestination());
+        public IActionResult Index()
+        {
+          var values=  destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet] 
+        public IActionResult DestinationDetails(int id)
+        {
+            var values=destinationManager.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult DestinationDetail(Destination d)
+        {
+            return View();
+        }
+    }
+}
