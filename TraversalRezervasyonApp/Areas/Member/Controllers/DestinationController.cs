7df539324f94d77703e568901e9c7b8f54ab24c7
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-19 18:15:39 +0300

    Member Sayfasında Verilerin Listelenmesi

---
+++ TraversalRezervasyonApp/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalRezervasyonApp.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestination());
+        public IActionResult Index()
+        {
+            var values= destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
