8eaf12aa557f85a8f2804b4f8e99602bc73ccfdd
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-19 21:59:09 +0300

    Identity ile Profil & Resim Bilgisi Güncelleme

--- TraversalRezervasyonApp/Areas/Member/Controllers/ProfileController.cs
+++ TraversalRezervasyonApp/Areas/Member/Controllers/ProfileController.cs
@@ -26,5 +26,30 @@ namespace TraversalRezervasyonApp.Areas.Member.Controllers
             userEditViewModel.mail = values.Email;
             return View(userEditViewModel);
         }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user =await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Image!=null) 
+            {
+                var resource=Directory.GetCurrentDirectory();
+                var extension= Path.GetExtension(p.Image.FileName);
+                var imageName=Guid.NewGuid()+extension;
+                var saveLocation = resource + "/wwwroot/userimages/" + imageName;
+                var stream=new FileStream(saveLocation, FileMode.Create);
+                await p.Image.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            user.Name = p.name;
+            user.Surname = p.surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user,p.password);
+            var result= await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("SignIn","Login");
+            }
+            return View();
+        }
+
     }
 }
