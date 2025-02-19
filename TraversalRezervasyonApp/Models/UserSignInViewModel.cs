c3b1e4426182dd992bf4dff63326cd73bd5d9d82
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-19 18:54:37 +0300

    Login with Identity

---
+++ TraversalRezervasyonApp/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalRezervasyonApp.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Lütfen kullanıcı adını boş geçmeyin!!!")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Lütfen  şifre kısmını boş geçmeyin!!!")]
+        public string password { get; set; }
+    }
+}
