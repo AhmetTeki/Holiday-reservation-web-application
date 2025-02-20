98ea9db9b4035197a82d190c92e0bfa6e65f6c39
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-20 16:05:09 +0300

    reservation page

--- TraversalRezervasyonApp/Areas/Member/Controllers/ReservationController.cs
+++ TraversalRezervasyonApp/Areas/Member/Controllers/ReservationController.cs
@@ -1,15 +1,23 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
 
 namespace TraversalRezervasyonApp.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestination());
+        ReservationManager reservationManager = new ReservationManager(new EFReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
         }
+
+
         public IActionResult MyOldReservation()
         {
             return View();
@@ -17,12 +25,21 @@ namespace TraversalRezervasyonApp.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList() select new SelectListItem
+            {
+                Text=x.City,
+                Value=x.DestinationID.ToString(),
+            } ).ToList();
+            ViewBag.v = values;
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 8;
+
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
