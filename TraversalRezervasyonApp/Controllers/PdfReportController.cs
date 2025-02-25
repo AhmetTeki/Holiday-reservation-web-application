79dd54d8078b4442fd3180619aa3c869acabd7c8
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-26 00:08:38 +0300

    Pdf Rapor(yapamadım)

---
+++ TraversalRezervasyonApp/Controllers/PdfReportController.cs
@@ -0,0 +1,20 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalRezervasyonApp.Controllers
+{
+    public class PdfReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StaticPdfReport()
+        {
+            return View();
+
+
+        }
+    }
+}
