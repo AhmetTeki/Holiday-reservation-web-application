78ae54c684bef6362b88a35c2db5756342d9cde2
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-02-26 23:12:18 +0300

    Ajax İşlemleri 1

---
+++ TraversalRezervasyonApp/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalRezervasyonApp.Models;
+
+namespace TraversalRezervasyonApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(cities);
+            return Json(jsonCity);
+        }
+
+        public static List<CityClass> cities = new List<CityClass>
+        {
+            new CityClass
+            {
+                CityID = 1,
+                CityName="Gerger",
+                CityCountry="Türkiye"
+            },
+            new CityClass
+            {
+                CityID = 2,
+                CityName="Sultangazi",
+                CityCountry="Türkiye"
+            },
+            new CityClass
+            {
+                CityID = 3,
+                CityName="Kabil",
+                CityCountry="Afganistan"
+            }
+        };
+    }
+}
